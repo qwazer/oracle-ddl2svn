7910db4b2cc98e0f390a7de9f93d1c2346425229
resheto@gmail.com <resheto@gmail.com@95ace979-9dd3-e25e-2026-d47d29152980>
2011-03-06 14:50:35 +0000

    add clean command

--- trunk/scheme2ddl/scripts/oracle-ddl2svn.bat
+++ trunk/scheme2ddl/scripts/oracle-ddl2svn.bat
@@ -15,6 +15,9 @@ rem you can overwrite this parameters
 set DB_URL=%1
 set OUTPUT_DIR=%2
 
+rem delete all files from output directory exept system files
+rem this command must keep on disk svn meta information stored in .svn folders
+del %OUTPUT_DIR% /Q /S /A-S
 java -jar scheme2ddl.jar -url %DB_URL% -output %OUTPUT_DIR%
 call autocommit %OUTPUT_DIR% 
 
