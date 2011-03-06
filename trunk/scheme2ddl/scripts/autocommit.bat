2a97a1615c4b7cb79618d42ab3a4d91b16b757f9
resheto@gmail.com <resheto@gmail.com@95ace979-9dd3-e25e-2026-d47d29152980>
2011-03-06 14:53:29 +0000

    commit message

--- trunk/scheme2ddl/scripts/autocommit.bat
+++ trunk/scheme2ddl/scripts/autocommit.bat
@@ -3,4 +3,4 @@ rem script for auto commiting directory tree changes in svn
 rem directory must be under version control
 for /f "tokens=2*" %%i in ('svn status %1 ^| find "?"') do svn add "%%i"
 for /f "tokens=2*" %%i in ('svn status %1 ^| find "!"') do svn delete "%%i"
-svn commit -m "Automatic commit" %1
+svn commit -m "Automatic commit by oracle-ddl2svn" %1
