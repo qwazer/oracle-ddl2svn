bf1cdfbc3a160cac7300f2447794bf2235e53758
resheto@gmail.com <resheto@gmail.com@95ace979-9dd3-e25e-2026-d47d29152980>
2011-04-16 13:54:50 +0000

    add test db connection option

--- trunk/scheme2ddl/scripts/oracle-ddl2svn.bat
+++ trunk/scheme2ddl/scripts/oracle-ddl2svn.bat
@@ -19,12 +19,22 @@ set SVN_USER=%3
 set SVN_PASS=%4
 set COMMIT_MESSAGE="automatic commit by oracle-ddl2svn"
 
+rem test db connection,
+rem if db connect is fail, do not perform any filesystem operation, only clean tmp file end exit
+set tempfile=connection.tmp
+del %tempfile%
+java -jar scheme2ddl.jar --test-connection -url %DB_URL% > %tempfile% 2>&1
+find "FAIL connect to" %tempfile%
+if not errorlevel 1 goto :exit
+
+:runScript
 rem delete all files from output directory exept system files
 rem this command must keep on disk svn meta information stored in .svn folders
 del %OUTPUT_DIR% /Q /S /A-S
 java -jar scheme2ddl.jar -url %DB_URL% -output %OUTPUT_DIR%
-rem call autocommit %OUTPUT_DIR% 
 for /f "tokens=2*" %%i in ('svn status %OUTPUT_DIR% ^| find "?"') do svn add "%%i"
 for /f "tokens=2*" %%i in ('svn status %OUTPUT_DIR% ^| find "!"') do svn delete "%%i"
-rem svn commit -m %COMMIT_MESSAGE% %OUTPUT_DIR%
 svn commit -m %COMMIT_MESSAGE% %OUTPUT_DIR%  --non-interactive --no-auth-cache --username %SVN_USER% --password %SVN_PASS%
+
+:exit
+del %tempfile%
