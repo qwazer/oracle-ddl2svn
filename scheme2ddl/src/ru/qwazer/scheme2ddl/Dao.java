19af0771d3c337fd04eb0b19e0484c703e6ef396
resheto@gmail.com <qwazer@users.noreply.github.com>
2011-08-01 05:21:16 +0000

    update copyright info

--- scheme2ddl/src/ru/qwazer/scheme2ddl/Dao.java
+++ scheme2ddl/src/ru/qwazer/scheme2ddl/Dao.java
@@ -1,5 +1,5 @@
 /*
- *    Copyright (c) 2011.
+ *    Copyright (c) 2011 Reshetnikov Anton aka qwazer
  *
  *    Licensed under the Apache License, Version 2.0 (the "License");
  *    you may not use this file except in compliance with the License.
