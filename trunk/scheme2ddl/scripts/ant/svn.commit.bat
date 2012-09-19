50d93559d22a43f6565b8eb0968f29b8eb00dffc
resheto@gmail.com <resheto@gmail.com@95ace979-9dd3-e25e-2026-d47d29152980>
2012-09-19 13:19:13 +0000

    ant scripts for windows

---
+++ trunk/scheme2ddl/scripts/ant/svn.commit.bat
@@ -0,0 +1,12 @@
+@echo off
+
+set COMMIT_MESSAGE="automatic commit by oracle-ddl2svn"
+
+
+echo =========  start of svn commit  %date% %time% ==============
+for /f "tokens=2*" %%i in ('svn status %OUTPUT_DIR% ^| find "?"') do svn add "%%i"
+for /f "tokens=2*" %%i in ('svn status %OUTPUT_DIR% ^| find "!"') do svn delete "%%i"
+svn commit -m %COMMIT_MESSAGE% %OUTPUT_DIR%  --non-interactive -no-auth-cache --username %SVN_USER% --password %SVN_PASSWORD%
+echo =========  end of svn commit %date% %time% ==============
+
+
