61ddbd6d8070f6ba05ce3c7d77654b5c2889f33d
resheto@gmail.com <resheto@gmail.com@95ace979-9dd3-e25e-2026-d47d29152980>
2012-09-24 13:04:27 +0000

    windows svn.commit.bat

--- trunk/scheme2ddl/scripts/ant/svn.commit.bat
+++ trunk/scheme2ddl/scripts/ant/svn.commit.bat
@@ -6,7 +6,7 @@ set COMMIT_MESSAGE="automatic commit by oracle-ddl2svn"
 echo =========  start of svn commit  %date% %time% ==============
 for /f "tokens=2*" %%i in ('svn status %OUTPUT_DIR% ^| find "?"') do svn add "%%i"
 for /f "tokens=2*" %%i in ('svn status %OUTPUT_DIR% ^| find "!"') do svn delete "%%i"
-svn commit -m %COMMIT_MESSAGE% %OUTPUT_DIR%  --non-interactive -no-auth-cache --username %SVN_USER% --password %SVN_PASSWORD%
+svn commit -m %COMMIT_MESSAGE% %OUTPUT_DIR%  --non-interactive --no-auth-cache --username %SVN_USER% --password %SVN_PASSWORD%
 echo =========  end of svn commit %date% %time% ==============
 
 
